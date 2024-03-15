da3407c225ba415890c5a83221d12aa9fb71b7c1
learnerNitishK <ntshkumar95@gmail.com>
2024-03-15 21:36:12 +0530

    Added exit messagebox

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import tkinter as tk
+import threading
+import notification as nf
+from tkinter import messagebox
+
+def start_notification():
+    threading.Thread(target=nf.notif).start()
+
+def on_closing():
+    if messagebox.askyesno(title='Quit?', message='Do you really want to quit?'):
+        window.destroy()
 
 window = tk.Tk()
 window.configure(bg="lightblue")
 window.geometry("800x600")
 window.title("Notification centre")
 
-notification = tk.Button(window, text="Notification's", font=('Arial', 12))
+notification = tk.Button(window, text="Notification's", font=('Arial', 12), command=start_notification)
 notification.place(x=10, y=40)
 
 lable = tk.Label(window, text="Home", font=('Arial', 18))
@@ -17,5 +27,5 @@ text1.pack(padx=5, pady=80)
 save = tk.Button(window, text="Save Task", font=('Areal', 12))
 save.pack(padx=5, pady=10)
 
-
+window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
--- notification.py
+++ notification.py
@@ -2,11 +2,13 @@ import datetime
 import time 
 from plyer import notification
 
-while True:
-    notification.notify(
-        title = "To Do List {}".format(datetime.date.today()),
-        message = "1. Make and integrate python project\n2. Create a Three tier project\n3. Create a CI/CD pipeline project.",
-        app_icon = "Notification app/notify.ico",
-        timeout = 5
-    )
-    time.sleep(10)
+def notif():
+    while True:
+        notification.notify(
+            title = "To Do List {}".format(datetime.date.today()),
+            app_name = "My App",
+            message = "1. Make and integrate python project\n2. Create a Three tier project\n3. Create a CI/CD pipeline project.",
+            app_icon = "notify.ico",
+            timeout = 5
+        )
+        time.sleep(10)
